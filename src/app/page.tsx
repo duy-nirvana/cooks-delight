bbbb70da5c1df0bd169a1743c92523059822bd40
duyTran <duytran@gleads.vn>
2025-04-16 16:41:42 +0700

    add hero text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,15 @@ export default function Home() {
     <div className="h-[577px] overflow-hidden rounded-4xl bg-[url(/hero-thumbnail.jpg)] bg-cover lg:bg-bottom bg-left relative">
       <div className="absolute inset-0 bg-dark/60"></div>
       <div className="absolute top-1/2 left-1/2 -translate-1/2 w-full text-center">
-        <h1 className="text-primary">
+        <h1 className="text-primary mb-3">
           UNLEASH CULINARY <br />
           EXCELLENCE
         </h1>
+        <p className="paragraph-1 text-primary">
+          Explore a world of flavors, discover <br />
+          handcrafted recipes, and let the aroma of <br />
+          our passion for cooking fill your kitchen
+        </p>
       </div>
     </div>
   );
